7e37ee018a40e93b928aa66b702a53603c25a86a
anniekoshy1 <koshya@email.sc.edu>
2024-09-22 19:43:24 -0400

    part4

---
+++ csce242/projects/part4/script.js
@@ -0,0 +1,5 @@
+<script>
+    // Toggle the 'show' class to display/hide the navigation menu on mobile
+    document.querySelector('.nav-toggle').addEventListener('click', function() {
+        document.querySelector('nav ul').classList.toggle('show')});
+</script>
