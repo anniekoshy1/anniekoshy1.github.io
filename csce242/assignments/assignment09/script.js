a8bf85f30617385aea04da35b18dc850cbb11da9
anniekoshy1 <koshya@email.sc.edu>
2024-10-06 13:37:56 -0400

    assignment09

--- csce242/assignments/assignment09/script.js
+++ csce242/assignments/assignment09/script.js
@@ -36,7 +36,7 @@ class Bird {
 // Array of Bird objects
 const birds = [
     new Bird('Hummingbird', '2.5 inches', '3-5 years', 'Nectar (Sugar water)', 'Trees', "They're nicknamed 'Hummers'", 'assignments/assignment09/images/hummingbird.jpg'),
-    new Bird('Blue Jay', '9-12 inches', '7 years', 'Seeds, insects', 'Woodlands', 'Known for their intelligence', 'path/to/bluejay.jpg'),
+    new Bird('Blue Jay', '9-12 inches', '7 years', 'Seeds, insects', 'Woodlands', 'Known for their intelligence', 'assignments/assignment09/images/bluejay.jpg'),
     new Bird('Cardinal', '8-9 inches', '3 years', 'Seeds, fruit', 'Gardens', 'They’re often seen in pairs', 'path/to/cardinal.jpg'),
     new Bird('Robin', '9-11 inches', '2 years', 'Insects, berries', 'Forests', 'They’re a sign of spring', 'path/to/robin.jpg')
 ];
