7b2bc489ce0414810acacbec0d066f3c6f4f5d35
anniekoshy1 <koshya@email.sc.edu>
2024-10-06 22:28:36 -0400

    main

--- csce242/projects/part5/script.js
+++ csce242/projects/part5/script.js
@@ -5,17 +5,3 @@ const navLinks = document.querySelector('.nav-links');
 burger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
-// Toggle navigation menu on mobile
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
-
-burger.addEventListener('click', () => {
-  navLinks.classList.toggle('nav-active');
-});
-// Toggle navigation menu on mobile
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
-
-burger.addEventListener('click', () => {
-  navLinks.classList.toggle('nav-active');
-});
